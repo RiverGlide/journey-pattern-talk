38c206e0708bcd03e0f70220b2f985dd211c14c9
antonymarcano <antony@antonymarcano.com>
2013-10-21 18:44:19 +0100

    Reducing the noise in the FindBy declaration

--- petclinic-acceptance/src/test/java/com/riverglide/journeypatterntalk/seleniumpageobject/HomePage.java
+++ petclinic-acceptance/src/test/java/com/riverglide/journeypatterntalk/seleniumpageobject/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.How;
 
 public class HomePage {
 
-    @FindBy(how = How.CSS, using="h2")
+    @FindBy(css="h2")
     private WebElement welcomeMessage;
 
     public String getWelcomeMessage() {
